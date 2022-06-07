86b6e3f6aa273f0f76a729d78a9b341a3b421f91
Vladimir <fok.vov@gmail.com>
2022-06-07 20:07:05 +0300

    Add task1

--- FirstHW/task1/Program.cs
+++ FirstHW/task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Введите первое число: ");
+string firstNumber = Console.ReadLine();
+int firstInt = int.Parse(firstNumber);
+
+System.Console.WriteLine("Введите второе число: ");
+string secondNumber = Console.ReadLine();
+int secondInt = int.Parse(secondNumber);
+
+if (firstInt > secondInt)
+{
+    System.Console.WriteLine("max = " + firstInt);
+    System.Console.WriteLine("min = " + secondInt);
+}
+else
+{
+    System.Console.WriteLine("max = " + secondInt);
+    System.Console.WriteLine("min = " + firstInt); 
+}
