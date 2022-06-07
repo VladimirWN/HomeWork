a093aedea4a1892f55a1290cec56b268359a4612
Vladimir <fok.vov@gmail.com>
2022-06-07 20:36:51 +0300

    Add task4

---
+++ FirstHW/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.Write("Введите число: ");
+string Number = Console.ReadLine();
+int numInt = int.Parse(Number);
+
+int index = 2;
+
+if (index > numInt) System.Console.WriteLine("Таких чисел нет");
+else
+{
+    while (index < numInt)
+    {
+        System.Console.Write(index);
+        index += 2;
+        if (index < numInt) System.Console.Write(", ");
+        else if (index == numInt) System.Console.Write($", {index}");
+    }
+
+}
