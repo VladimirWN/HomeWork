53dec1721d5f6af063b45faace325c9b1a755747
Vladimir <fok.vov@gmail.com>
2022-06-20 11:01:10 +0300

    add fourth hm

---
+++ FourthHM/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int a = Prompt("Введите число A: ");
+int b = Prompt("Введите число B: ");
+
+int result = 1;
+int index = 1;
+
+
+while (index <= b)
+{
+    result = result * a;
+    index++;
+}
+
+System.Console.WriteLine($"Число {a} в степени {b} равно: {result}.");
---
+++ FourthHM/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+// 6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
+
+
+int[] NewArray()
+{
+    int[] array = new int[8];
+
+    for(int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int[] array = NewArray();
+PrintArray(array);
