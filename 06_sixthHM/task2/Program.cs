a81d8db5599fdff6343afc5b858ee5e5e6a5389d
Vladimir <fok.vov@gmail.com>
2022-06-28 13:38:04 +0300

    add 6_sixthHM

---
+++ 06_sixthHM/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+(double, double) SearchIntersection(double b1, double k1, double b2, double k2)
+{
+    double xResult = 0.0;
+    double yResult = 0.0;
+    xResult = (b2 - b1) / (k1 - k2);
+    yResult = k1 * xResult + b1;
+    return (xResult, yResult);
+}
+
+
+double b1 = Prompt("Введите b1: ");
+double k1 = Prompt("Введите k1: ");
+double b2 = Prompt("Введите b2: ");
+double k2 = Prompt("Введите k2: ");
+
+if (k1 == k2) System.Console.WriteLine("Прямые параллельны.");
+else
+{
+    (double x, double y) = SearchIntersection(b1, k1, b2, k2);
+    System.Console.WriteLine($"Точка пересечения [{x}, {y}].");
+}
